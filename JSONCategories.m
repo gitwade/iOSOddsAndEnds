1abce6e516d8783f858b862544cb98e75ff80fd4
wade <gitwade@gmail.com>
2012-10-22 15:24:31 -0400

    Adding JSONCategories
    Convenient categories to parse JSON objects to and from strings.

---
+++ JSONCategories.m
@@ -0,0 +1,60 @@
+//
+//  JSONCategories.m
+//
+//    Copyright (c) 2012 Wade Simmons
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+//    and associated documentation files (the "Software"), to deal in the Software without restriction,
+//    including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//    and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+//    subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all copies or substantial
+//    portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+//    LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+
+#import "JSONCategories.h"
+
+@implementation NSObject (NSObject_NSJSONSerialization)
+
+- (NSString *)JSONRepresentation
+{
+    if ([NSJSONSerialization isValidJSONObject:self])
+    {
+        NSError *jsonParsingError = nil;
+//        NSData *JSONData = [NSJSONSerialization dataWithJSONObject:self options:NSJSONWritingPrettyPrinted error:&jsonParsingError];
+        NSData *JSONData = [NSJSONSerialization dataWithJSONObject:self options:0 error:&jsonParsingError];
+        NSString *json = [[NSString alloc] initWithData:JSONData encoding:NSUTF8StringEncoding];
+        if (jsonParsingError)
+        {
+            NSLog(@"JSON Parsing Error: %@", jsonParsingError);
+        }
+        return json;
+    }
+    return nil;
+}
+
+@end
+
+@implementation NSString (NSString_NSJSONSerialization)
+
+- (id)JSONValue
+{
+    NSError *jsonParsingError = nil;
+    NSData *JSONData = [self dataUsingEncoding:NSUTF8StringEncoding];
+    id json = [NSJSONSerialization JSONObjectWithData:JSONData options:NSJSONReadingMutableContainers | NSJSONReadingMutableLeaves error:&jsonParsingError];
+    if (jsonParsingError)
+    {
+        NSLog(@"JSON Parsing Error: %@", jsonParsingError);
+    }
+    return json;
+}
+
+@end
